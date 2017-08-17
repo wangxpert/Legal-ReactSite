f3883aeb7131ccbceb0f124751fde5c0d4dd215a
“Zootopia106” <pro.zootopia106@gmail.com>
2017-08-18 07:13:00 +0900

    Add animation to Legal topic, forms header

--- client/modules/LegalForms/components/PageHeader/index.js
+++ client/modules/LegalForms/components/PageHeader/index.js
@@ -9,26 +9,26 @@ import imgPrint  from '../../../../assets/print.png';
 
 function PageHeader(props) {
   return (
-    <div className={ styles.container }>
+    <div className={ `${ styles.container }` }>
       <img className={ styles['back-img'] } src={ imgBack } />
       <div className={ styles.overlay }>
         <div className="container">
-          <div className={ styles.title }>
+          <div className={ `${ styles.title } wow fadeInLeft` }  data-wow-delay="0.3s" data-wow-duration="0.6s">
             Legal Forms
           </div>
-          <div className={ styles.description }>
+          <div className={ `${styles.description} wow fadeInRight` } data-wow-delay="0.5s" data-wow-duration="0.6s">
             Welcome to the quick and simple solution to legal forms.
           </div>
-          <div className={ `row ${ styles.content }` }>
-            <div className="col-xs-4">
+          <div className={ `row ${ styles.content } wow` } data-wow-delay="1.6s">
+            <div className="col-xs-4 wow flipInX" data-wow-delay="1.4s">
               <img className={ styles.icon } src={ imgClock } />
               <div>Quick and easy</div>
             </div>
-            <div className="col-xs-4">
+            <div className="col-xs-4 wow flipInX" data-wow-delay="1.6s">
               <img className={ styles.icon } src={ imgCheck } />
               <div>Reliable results</div>
             </div>
-            <div className="col-xs-4">
+            <div className="col-xs-4 wow flipInX" data-wow-delay="1.8s">
               <img className={ styles.icon } src={ imgPrint } />
               <div>Ready for print</div>
             </div>
--- client/modules/LegalTopics/components/PageHeader/index.js
+++ client/modules/LegalTopics/components/PageHeader/index.js
@@ -13,22 +13,22 @@ function PageHeader(props) {
       <img className={ styles['back-img'] } src={ imgBack } />
       <div className={ styles.overlay }>
         <div className="container">
-          <div className={ styles.title }>
+          <div className={ `${ styles.title } wow fadeInLeft` }  data-wow-delay="0.3s" data-wow-duration="0.6s">
             Legal Topics
           </div>
-          <div className={ styles.description }>
+          <div className={ `${styles.description} wow fadeInRight` } data-wow-delay="0.5s" data-wow-duration="0.6s">
             Our Mavens are here to help! Scroll below to get started.
           </div>
-          <div className={ `row ${ styles.content }` }>
-            <div className="col-xs-4">
+          <div className={ `row ${ styles.content } wow` } data-wow-delay="1.6s">
+            <div className="col-xs-4 wow flipInX" data-wow-delay="1.4s">
               <img className={ styles.icon } src={ imgFree } />
               <div>Free to use</div>
             </div>
-            <div className="col-xs-4">
+            <div className="col-xs-4 wow flipInX" data-wow-delay="1.6s">
               <img className={ styles.icon } src={ imgClock } />
               <div>Quick and easy</div>
             </div>
-            <div className="col-xs-4">
+            <div className="col-xs-4 wow flipInX" data-wow-delay="1.8s">
               <img className={ styles.icon } src={ imgCheck } />
               <div>Reliable results</div>
             </div>
